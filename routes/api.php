57433ccf01747a2afb4a81f1207e1b3cac2e94ba
Maurício Junior <junior9771@gmail.com>
2021-06-12 17:45:37 -0300

    chore: add register route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
 });
+
+Route::get('foo', [\App\Http\Controllers\TestController::class, 'foo']);
