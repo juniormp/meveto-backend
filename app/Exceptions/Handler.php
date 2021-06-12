8c46150d84b17ebd029c458f42c0e39958a1da12
Maurício Junior <junior9771@gmail.com>
2021-06-12 17:45:19 -0300

    chore: add validations on exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use App\Http\ApiCode;
+use App\Http\HttpCode;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use MarcinOrlowski\ResponseBuilder\ResponseBuilder;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -35,7 +39,19 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            return ResponseBuilder::asError(101)
+                ->withData($exception->errors())
+                ->withHttpCode(422)
+                ->build();
+        }
+
+        return parent::render($request, $exception);
     }
 }
