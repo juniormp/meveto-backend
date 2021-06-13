da57d0784d4ff4de849ef653399b17e4ce7505ae
Maurício Junior <junior9771@gmail.com>
2021-06-13 11:14:46 -0300

    chore: add storages on user domain model

--- app/Domain/Auth/User.php
+++ app/Domain/Auth/User.php
@@ -3,15 +3,19 @@
 namespace App\Domain\Auth;
 
 use App\Domain\Encryption\Key;
+use App\Domain\Storage\Storage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Collection;
 
 /**
  * @property int $id
  * @property string $username
  * @property Key $key
+ * @property Collection $storages
  */
 class User extends Model
 {
@@ -24,5 +28,10 @@ class User extends Model
     public function key(): HasOne
     {
         return $this->hasOne(Key::class);
+    }
+
+    public function storages(): HasMany
+    {
+        return $this->hasMany(Storage::class);
     }
 }
