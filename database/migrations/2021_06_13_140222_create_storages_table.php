ff6c90773808e86d56128851f1e9397fddb51090
Maurício Junior <junior9771@gmail.com>
2021-06-13 11:11:53 -0300

    chore: create storage table

---
+++ database/migrations/2021_06_13_140222_create_storages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoragesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('storages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('secret_name');
+            $table->text('encrypted_secret');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('storages');
+    }
+}
